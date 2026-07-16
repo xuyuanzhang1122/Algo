565ec353cdd864780682b34c374b378b853c799f
xuyuanzhang1122 <xumy0531@163.com>
2026-07-16 03:15:30 +0000

    new

--- cycle-node.cpp
+++ cycle-node.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
 typedef int ElemType;
-
 typedef struct LNode
 {
     ElemType data;
@@ -11,18 +9,27 @@ typedef struct LNode
 }LNode, *LinkList;
 /*初始化链表*/
 bool InitList(LinkList &L){
-    L = (LNode *)malloc(sizeof(LNode));
+    L = (LNode *)malloc(sizeof(LNode)); //申请新内存
     if(L == NULL){
-        return false;
+        return false;   //代码健壮性
     }
-    L->next = L;
+    L->next = L;    //让链表的尾指针指向表头
     return true;
 }
 /*判断链表是否为空*/
 bool Empty(LinkList L){
-    if(L->next == L){
+    if(L->next == L){   //如果尾指针指向表头
         return true;
     }else{
+        return false;
+    }
+}
+/*判断P节点下一个节点是否为尾节点*/
+bool isTail(LinkList L, LNode *p){
+    if(p->next == L){   //如果P（当前）指针指向表头
+        return true;
+    }
+    else{
         return false;
     }
 }
